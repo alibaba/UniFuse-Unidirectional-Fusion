780876f2fda4c938e978beffe3f732730dd4f870
Hualie Jiang <jianghualie0@gmail.com>
2021-09-02 14:09:49 +0800

    Update stitching_Matterport3D.m
    To avoid some possible problems.

--- UniFuse/Matterport3D/stitching_Matterport3D.m
+++ UniFuse/Matterport3D/stitching_Matterport3D.m
@@ -1,18 +1,19 @@
 %% stitching Matterport3D panorama images and depth  maps
 clear; close all;
+add_path;
 
-source_dir = '/home/wolian/disk2/Matterport';
-target_dir = '/home/wolian/disk1/Matterport3D';
+source_dir = '/home/hljiang/disk8t/datasets/Matterport3D/v1/scans/';
+target_dir = '/home/hljiang/disk8t/processed/Matterport3D/';
 
 listing = dir(source_dir);
 listing(ismember( {listing.name}, {'.', '..'})) = [];
 
 for i = 1:length(listing)
     
-    if ~exist(strcat(target_dir, '/', listing(i).name), 'dir')
-        mkdir(strcat(strcat(target_dir, '/', listing(i).name)))
-    else
+    if exist(strcat(target_dir, '/', listing(i).name), 'dir') || ~listing(i).dir
         continue
+    else
+        mkdir(strcat(strcat(target_dir, '/', listing(i).name)))
     end
     
     stitch(strcat(source_dir, '/', listing(i).name), strcat(target_dir, '/', listing(i).name), listing(i).name);
